5ee56f95510050c72a74de5f5639c825b3fe8d02
gauravgargtech <gauravgargtech2@gmail.com>
2020-10-06 21:46:20 +1100

    grunt

---
+++ gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    // define source files and their destinations
+    uglify: {
+      files: {
+        src: "public/js/*.js", // source files mask
+        dest: "public/jsm/", // destination folder
+        expand: true, // allow dynamic building
+        flatten: true, // remove all unnecessary nesting
+        ext: ".min.js", // replace .js to .min.js
+      },
+    },
+    cssmin: {
+      target: {
+        files: [
+          {
+            expand: true,
+            cwd: "public/css",
+            src: ["*.css", "!*.min.css"],
+            dest: "public/cssm",
+            ext: ".min.css",
+          },
+        ],
+      },
+    },
+    watch: {
+      js: { files: "js/*.js", tasks: ["uglify"] },
+      css: { files: "css/*.css", tasks: ["cssmin"] },
+    },
+  });
+
+  // load plugins
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+
+  // register at least this one task
+  grunt.registerTask("default", ["uglify", "cssmin"]);
+};
